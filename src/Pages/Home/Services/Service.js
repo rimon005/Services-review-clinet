2f684a32d4b8c86efb16f6991cd95ce0c8459a34
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-11-08 13:20:56 +0600

    add some service

---
+++ src/Pages/Home/Services/Service.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Services/Servies.css'
+
+const Service = ({service}) => {
+    console.log(service);
+    const { description , title , img , price , _id} = service;
+    return (
+        <div className="card card-compact bg-base-100 shadow-xl rounded-none p-5 border">
+            <figure className='service-img'><img src={img} alt="" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <p className='description'>{description}</p>
+                <div className='flex items-center'>
+                    <p className='text-xl font-bold price'>Price: $ {price}</p>
+                    <Link to={`/serviceDetails/${_id}`} className='btn contact-btn rounded-none border-none text-white'>
+                        <button>
+                            Details
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Service from './Service';
+import './Servies.css'
+const Services = () => {
+    const [services , setServices ] = useState([])
+    useEffect(()=> {
+        fetch('http://localhost:5000/services')
+        .then(res => res.json())
+        .then(data => {
+           setServices(data)
+        })
+    },[])
+
+    return (
+        <div>
+            <div className='text-center services'>
+                <h1 className='text-3xl mb-3'>Here Our Best Work</h1>
+                <h1 className='text-5xl mb-3'>How Can We Help You</h1>
+                <hr  className='w-16 mx-auto'/>
+            </div>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5 py-12'>
+                {
+                    services.map(service => <Service
+                    key={service._id}
+                    service={service}
+                    ></Service>)
+                }
+            </div>
+            <Link className=' flex justify-center'>
+                <button className='btn contact-btn rounded-none border-none text-white'>More service</button>
+            </Link>
+        </div>
+    );
+};
+
+export default Services;
