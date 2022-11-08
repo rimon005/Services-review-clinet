a3a8477f53d422504173bc8df0e75c137ab35e35
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-11-08 11:19:05 +0600

    login auth

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,24 +1,40 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import loginImg from '../../../assets/istockphoto-1281150061-612x612.jpg'
+import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
 import './Login.css'
 
 const Login = () => {
+    const {login} = useContext(AuthContext)
+    const handleLogin = event => {
+        event.preventDefault()
+        const form = event.target ;
+        const email = form.email.value;
+        const password = form.password.value;
+        login(email , password)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+
+        })
+        .catch(e => console.error(e))
+    }
     return (
         <div className="hero w-full mb-12">
             <div className="hero-content flex-col lg:flex-row">
                 <div className="text-center lg:text-left mr-14">
                     <img className='w-4/5' src={loginImg} alt="" />
                 </div>
-                <form className="card login-card p-6 flex-shrink-0 w-full max-w-sm rounded-none bg-base-100">
+                <form onSubmit={handleLogin} className="card login-card p-6 flex-shrink-0 w-full max-w-sm rounded-none bg-base-100">
                     <h1 className="text-3xl text-center mt-2 font-bold">Login now!</h1>
                     <div className="card-body">
                         <div className="form-control">
-                            <input type="text" placeholder="email" name='email' required className="input rounded-none input-field" />
+                            <input type="email" placeholder="email" name='email' required className="input rounded-none input-field" />
                         </div>
                         <div className="form-control">
                             
-                            <input type="text" placeholder="password" name='password' required className="input input-field rounded-none" />
+                            <input type="password" placeholder="password" name='password' required className="input input-field rounded-none" />
                             <label className="label">
                                 <Link href="#" className="label-text-alt link link-hover">Forgot password?</Link>
                             </label>
