eab0364e1f99b3bc26907e3ec1a2dd80c671c11d
Asif Iqbal Rimon <ariyan8185@gmail.com>
2022-11-08 09:19:43 +0600

    create header

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const menuItems = <>
+        <li><Link> Home</Link></li>
+        <li><Link> About</Link></li>
+        <li><Link> Services</Link></li>
+        <li><Link> Contact </Link></li>
+        <li><Link> My Reviews </Link></li>
+        <li><Link> Login</Link></li>
+
+    </>
     return (
-        <div>
-            <h2>This is header</h2>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
+                </ul>
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img src="https://placeimg.com/80/80/people" />
+                        </div>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><Link> LogOut</Link></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
